c9c142703ace74dd075f46763d054223fc5c6835
Alexandru Gabriel Bradatan <alex.bradatan85@yahoo.it>
2021-10-29 17:32:50 +0200

    refactor(backend): refactor index and logger

--- server/index.js
+++ server/index.js
@@ -3,31 +3,30 @@ const app = express();
 const path = require('path');
 
 const http = require('http').createServer(app);
-const options = {};
 const session = require('express-session');
-const bodyParser = require('body-parser');
+
 const www = require('./routes/www');
-const logger = require('./routes/logger');
 
-// MIDDLEWARE
 app.use(express.json());
-app.use(express.urlencoded({'extended': false}));
+app.use(express.urlencoded({ extended: false }));
 
-app.use(session({
-  secret: 'cats',
-  name: 'sessionid',
-  resave: true,
-  // TODO: add 'secure' and a cookie session store suitable for production
-  saveUninitialized: true
-}));
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(
+  session({
+    secret: 'cats',
+    name: 'sessionid',
+    resave: true,
+    // TODO: add 'secure' and a cookie session store suitable for production
+    saveUninitialized: true
+  })
+);
 
-// ROUTES MIDDLEWARE
+app.use('/', (req, _, next) => {
+  console.log(req.protocol, req.method, 'request from', req.ip);
+  next();
+});
 if (process.env.NODE_ENV === 'production') {
   app.use('/', express.static(path.join(__dirname, 'dist')));
 }
-
-app.use('/', logger);
 app.use('/api', www);
 
 http.listen(3000, () => {
