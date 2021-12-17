96643854a647f97669ddef99728eab9af031ebb0
Alexandru Gabriel Bradatan <alex.bradatan85@yahoo.it>
2021-12-18 00:41:33 +0100

    fix: re-expose devServer to local network
    Due to a name clash between the `HOST` variable used in our `.env` and
    the one used by webpack, the devServer wasn't getting exposed to LAN
    like it would be expected. Change our variable to `HOSTNAME` to avoid
    the clash and restore functionality.
    
    Still relative to LAN access but too small for their own commit:
    
    1. Add a new IP altname containing a sample local IP (to be modified by user).
    2. Add a prompt for reminding the user to add said local IP.
    3. Add `-extensions v3_req` option to openssl so that altnames are
       effectiverly added to the certificate.

--- scripts/genCert.sh
+++ scripts/genCert.sh
@@ -18,7 +18,7 @@ while [ -n "$1" ]; do
       OUT=$2
       shift 2 ;;
     -h)
-      echo "Usage: genCert.sh -k KEY -p PEM [-e EXT|-o OUT|-h]"
+      echo "Usage: genCert.sh -k KEY -p PEM [-c CNF|-o OUT|-h]"
       echo "  -k  Root certificate private key"
       echo "  -p  Root certificate"
       echo "  -c  Openssl config (defaults to ./dev.psylean.it.cnf)"
@@ -32,6 +32,10 @@ while [ -n "$1" ]; do
 done
 
 if [ -d "$OUT" ] && [ -f "$CA_KEY" ] && [ -f "$CA_PEM" ] && [ -f "$CNF" ]; then
+  echo "Have you added you local IP address to the configuration file?"
+  echo "(If you don't want to use this feature just answer yes) [y/N]"
+  read -r answer
+  if [ answer != 'y' ] || [ answer != 'Y' ]; then exit -; fi
   openssl req -new \
     -newkey rsa:2048 \
     -keyout "$OUT/dev.psylean.it.key" \
@@ -44,7 +48,8 @@ if [ -d "$OUT" ] && [ -f "$CA_KEY" ] && [ -f "$CA_PEM" ] && [ -f "$CNF" ]; then
         -CAkey "$CA_KEY" \
         -CAcreateserial \
         -out "$OUT/dev.psylean.it.crt" \
-        -extfile "$CNF"
+        -extfile "$CNF" \
+        -extensions v3_req
 else
   echo "Check your paths..."
   exit 1
